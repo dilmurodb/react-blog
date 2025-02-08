fbc4236390e4da5770b9ac0d08bcfd7f4b86ddea
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-09 00:24:41 +0500

    feat: create all necessary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import Layout from "./Layout";
+import Home from "./Home";
+import About from "./About";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import Missing from "./Missing";
+
+import { Route, Routes, useNavigate } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='post'>
+            <Route index element={<NewPost />} />
+            <Route path=':id' element={<PostPage />} />
+          </Route>
+          <Route path='about' element={<About />}></Route>
+          <Route path='*' element={<Missing />}></Route>
+        </Route>
+      </Routes>
+   
   );
 }
 
