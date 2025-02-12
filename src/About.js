130b2f468b0930cfe8ff4d9818d6d84e4672660a
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-12 13:18:49 +0500

    feat: implement about section

--- src/About.js
+++ src/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main>About</main>
+    <main className="About">
+      <h2>About</h2>
+      <p>Blog app§</p>
+    </main>
   )
 }
 
--- src/Missing.js
+++ src/Missing.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Missing = () => {
   return (
-    <main>Missing</main>
+    <main className="Missing">
+      <h2>Post Not Found.</h2>
+      <p>Well, that's not good.</p>
+      <p>
+        <Link to='/'>Visit our Home Page</Link>
+      </p>
+    </main>
   )
 }
 
